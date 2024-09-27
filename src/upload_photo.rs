40d2fcc198e0abf8ef93b6cd58a27a0b5cbf841b
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2024-09-28 00:26:52 +0300

    Add XML error parsing

--- src/upload_photo.rs
+++ src/upload_photo.rs
@@ -6,7 +6,34 @@ use tokio::fs::read;
 #[serde(rename = "rsp")]
 struct UploadXMLAnswer {
     stat: String,
-    photoid: String,
+
+    #[serde(flatten)]
+    content: UploadXMLPayload,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+enum UploadXMLPayload {
+    #[serde(rename = "photoid")]
+    PhotoId {
+        #[serde(rename = "$value")]
+        value: String,
+    },
+
+    #[serde(rename = "err")]
+    Err { code: String, msg: String },
+}
+
+impl UploadXMLAnswer {
+    fn to_result(self) -> Result<String, FlickrError> {
+        match self.content {
+            UploadXMLPayload::PhotoId { value } => Ok(value),
+            UploadXMLPayload::Err { code, msg } => Err(FlickrError {
+                stat: self.stat,
+                code: code.parse().unwrap_or(0),
+                message: msg,
+            }),
+        }
+    }
 }
 
 impl PhotoRequestBuilder {
@@ -54,8 +81,47 @@ impl PhotoRequestBuilder {
             .send()
             .await?;
 
-        let answer: UploadXMLAnswer = serde_xml_rs::from_str(&request.text().await?)?;
+        let text = request.text().await?;
 
-        Ok(answer.photoid)
+        log::trace!("Upload response: {:?}", text);
+
+        let id = serde_xml_rs::from_str::<UploadXMLAnswer>(&text)?.to_result()?;
+
+        Ok(id)
     }
+}
+
+#[test]
+fn test_upload_answer_error() {
+    let anwser = r#"<?xml version="1.0" encoding="utf-8" ?><rsp stat="fail"><err code="5" msg="Filetype was not recognised"/></rsp>"#;
+
+    let answer = serde_xml_rs::from_str::<UploadXMLAnswer>(anwser).unwrap();
+
+    assert_eq!(
+        answer,
+        UploadXMLAnswer {
+            stat: "fail".to_string(),
+            content: UploadXMLPayload::Err {
+                code: "5".to_string(),
+                msg: "Filetype was not recognised".to_string()
+            }
+        }
+    );
+}
+
+#[test]
+fn test_upload_answer_ok() {
+    let anwser = r#"<?xml version="1.0" encoding="utf-8" ?><rsp stat="ok"><photoid>54026462270</photoid></rsp>"#;
+
+    let answer = serde_xml_rs::from_str::<UploadXMLAnswer>(anwser).unwrap();
+
+    assert_eq!(
+        answer,
+        UploadXMLAnswer {
+            stat: "ok".to_string(),
+            content: UploadXMLPayload::PhotoId {
+                value: "54026462270".to_string()
+            }
+        }
+    );
 }
